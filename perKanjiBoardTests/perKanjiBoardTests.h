e1a4f62e7c0c588cac3bff2eeb67787776c3e721
zermelozf <arnaud.rachez@gmail.com>
2012-01-02 14:28:40 +0900

    Initial Commit

---
+++ perKanjiBoardTests/perKanjiBoardTests.h
@@ -0,0 +1,17 @@
+//
+//  perKanjiBoardTests.h
+//  perKanjiBoardTests
+//
+//  Created by apple on 02/01/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface perKanjiBoardTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ perKanjiBoardTests/perKanjiBoardTests.m
@@ -0,0 +1,33 @@
+//
+//  perKanjiBoardTests.m
+//  perKanjiBoardTests
+//
+//  Created by apple on 02/01/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "perKanjiBoardTests.h"
+
+
+@implementation perKanjiBoardTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in perKanjiBoardTests");
+}
+
+@end
